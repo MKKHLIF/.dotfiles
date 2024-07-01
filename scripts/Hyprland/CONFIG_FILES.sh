4b6923c7ed1d6de1cd8c5b70aef8d164d28e86ef
Mayssam-Khlif <ms.khlif@gmail.com>
2024-07-01 23:21:58 +0100

    add nvim config init.lua

--- scripts/Hyprland/CONFIG_FILES.sh
+++ scripts/Hyprland/CONFIG_FILES.sh
@@ -10,6 +10,7 @@ rm -rf "$CONFIG_DIR/wofi"
 rm -rf "$CONFIG_DIR/hypr"
 rm -rf "$CONFIG_DIR/hyprpaper"
 rm -rf "$CONFIG_DIR/gammastep"
+rm -rf "$CONFIG_DIR/nvim"
 
 # Create configuration directories
 mkdir -p "$CONFIG_DIR/kitty"
@@ -17,6 +18,7 @@ mkdir -p "$CONFIG_DIR/wofi"
 mkdir -p "$CONFIG_DIR/hypr"
 mkdir -p "$CONFIG_DIR/hyprpaper"
 mkdir -p "$CONFIG_DIR/gammastep"
+mkdir -p "$CONFIG_DIR/nvim"
 
 # Link configuration files
 ln -sf "$GIT_DIR/configs/kitty.conf" "$CONFIG_DIR/kitty/kitty.conf"
@@ -25,4 +27,4 @@ ln -sf "$GIT_DIR/configs/kitty.conf" "$CONFIG_DIR/kitty/kitty.conf"
 # ln -sf "$GIT_DIR/configs/wofi/style.css" "$CONFIG_DIR/wofi/style.css"
 ln -sf "$GIT_DIR/configs/hyprland.conf" "$CONFIG_DIR/hypr/hyprland.conf"
 ln -sf "$GIT_DIR/configs/hyprpaper.conf" "$CONFIG_DIR/hyprpaper/hyprpaper.conf"
-ln -sf "$GIT_DIR/configs/gammastep/config.ini" "$CONFIG_DIR/gammastep/config.ini"
+ln -sf "$GIT_DIR/configs/nvim/init.lua" "$CONFIG_DIR/nvim/init.lua"
