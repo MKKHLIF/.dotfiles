e4041fe0198efbf4661437f17c509c16a2530a1a
Mayssam-Khlif <ms.khlif@gmail.com>
2024-08-15 17:44:51 +0100

    hardware uuid change

--- system/nix/sys/hardware-configuration.nix
+++ system/nix/sys/hardware-configuration.nix
@@ -8,25 +8,23 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/303a994f-7502-4f33-adcd-f6256c854368";
+    { device = "/dev/disk/by-uuid/1a985394-55e2-412c-b5b4-e97bae6b1265";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/3B55-7CE1";
+    { device = "/dev/disk/by-uuid/4EA5-C674";
       fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/0157b463-9e40-4db9-94f0-b47246bca24e"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
