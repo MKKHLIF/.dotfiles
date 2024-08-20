2d3d404cdcb41d739b7905989574de9e94365e66
Mayssam-Khlif <ms.khlif@gmail.com>
2024-08-20 21:20:22 +0100

    packages installer script

---
+++ system/arch/scripts/install_pkgs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Get the directory of the current script
+script_dir=$(dirname "$0")
+
+# Go one directory back
+cd "$script_dir/.."
+
+# Check if the "packages" directory exists
+if [ -d "packages" ]; then
+    # Loop through all the files in the "packages" directory
+    for package_file in packages/*; do
+        # Check if the file is a regular file (not a directory)
+        if [ -f "$package_file" ]; then
+            echo "Installing packages from $package_file..."
+            # Install the packages listed in the file
+            if command_exists pacman; then
+                sudo pacman -S --noconfirm --needed $(cat "$package_file")
+            elif command_exists apt-get; then
+                sudo apt-get install -y $(cat "$package_file")
+            elif command_exists yum; then
+                sudo yum install -y $(cat "$package_file")
+            else
+                echo "Unable to install packages. Please install the packages manually."
+            fi
+        fi
+    done
+else
+    echo "The 'packages' directory does not exist. No packages to install."
+fi
+
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
