351f865e5b073f5640f5059e68e8a77a783b5e4c
Mayssam-Khlif <ms.khlif@gmail.com>
2024-08-20 20:57:11 +0100

    welcome script

---
+++ system/arch/scripts/welcome.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# ANSI escape codes for formatting
+BOLD="\e[1m"
+RESET="\e[0m"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+WHITE="\e[37m"
+
+# Function to display the title
+display_title() {
+    clear
+    echo -e "${BOLD}${BLUE}"
+    cat << EOF
+         _   _    _____  ___    ___    _   _ 
+ / \_/ \( ) ( )  (  _  )|  _ \ (  _ \ ( ) ( )
+ |     || |/ /   | (_) || (_) )| ( (_)| |_| |
+ | (_) ||   (    (  _  )|    / | |  _ |  _  |
+ | | | || |\ \   | | | || |\ \ | (_( )| | | |
+ (_) (_)( ) (_)  (_) (_)(_) (_)(____/ (_) (_)
+        /(                                   
+       (__)                                  
+
+EOF
+    echo -e "${RESET}"
+}
+
+display_title
