c4e90ef6e59f4dbc74be5a629a97e969ee07faa6
Mayssam-Khlif <ms.khlif@gmail.com>
2024-08-21 18:18:47 +0100

    fix pkgs & scripts paths

--- system/arch/install.sh
+++ system/arch/install.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-./scripts/welcome.sh
-./scripts/git_identity.sh
-./scripts/configs_symlinks.sh
-./scripts/aur_helpers.sh
-./scripts/install_pkgs.sh
+scripts_dir=$HOME/.dotfiles/system/arch/scripts
 
+cd $scripts_dir
+
+message() {
+    echo -e "\e[1m\e[32m$1\e[0m"
+}
+
+./welcome.sh
+message "Setting Git Identity..."
+./git_identity.sh
+message "Setting AUR Helpers..."
+./aur_helpers.sh
+message "Creating Config Symlinks..."
+./configs_symlinks.sh
+message "Installing Packages..."
+./install_pkgs.sh
--- system/arch/scripts/install_pkgs.sh
+++ system/arch/scripts/install_pkgs.sh
@@ -1,35 +1,17 @@
 #!/bin/bash
 
-# Get the directory of the current script
-script_dir=$(dirname "$0")
+packages_dir=$HOME/.dotfiles/system/arch/packages
 
-# Go one directory back
-cd "$script_dir/.."
+for file in $packages_dir/*.list; do
+    if [ -f "$list_file"]; then 
+        file_name=$(basename "$file" .list)
+        echo " ===== Installing list: $file_name ===== "
+        while read -r pkg; do
+            paru -S --noconfirm --needed $pkg
+        done < $file
+    fi
+done
 
-# Check if the "packages" directory exists
-if [ -d "packages" ]; then
-    # Loop through all the files in the "packages" directory
-    for package_file in packages/*; do
-        # Check if the file is a regular file (not a directory)
-        if [ -f "$package_file" ]; then
-            echo "Installing packages from $package_file..."
-            # Install the packages listed in the file
-            if command_exists paru; then
-                paru -S --noconfirm --needed $(cat "$package_file")
-            elif command_exists yay; then
-                yay -S --noconfirm --needed $(cat "$package_file")
-            elif command_exists pacman; then
-                sudo pacman -S --noconfirm --needed $(cat "$package_file")
-            else
-                echo "Unable to find a compatible package manager. Please install the packages manually."
-            fi
-        fi
-    done
-else
-    echo "The 'packages' directory does not exist. No packages to install."
-fi
-
-# Function to check if a command exists
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
