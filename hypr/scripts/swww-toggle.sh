e53764d460405d7e34ee36aa0d14bc98b7462168
Mayssam-Khlif <khlif.mk@proton.me>
2024-11-09 13:52:42 +0100

    hyprland dots rewrite

--- hypr/scripts/swww-toggle.sh
+++
@@ -1,8 +0,0 @@
-
-# Check if swww is active
-if pgrep -x "swww-daemon" > /dev/null; then
-    swww kill
-else
-    swww-daemon &
-fi
-
--- hypr/scripts/waybar-toggle.sh
+++
@@ -1,8 +0,0 @@
-
-# Check if waybar is active
-if pgrep -x ".waybar-wrapped" > /dev/null; then
-    pkill .waybar-wrapped 
-else
-    waybar &
-fi
-
