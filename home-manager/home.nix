08dd7197269555e40f37fd7bb08515e792c2df18
mk <khlif.mk@proton.me>
2025-01-12 14:56:12 +0100

    move pkgs to home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -12,22 +12,21 @@ in
 
   imports = [
 
-    # Low Level Modules
+    # core modules
     ./modules/core/bluetooth.nix
 
-    # Desktop Environment 
+    # desktop environment 
     ./modules/desktop/i3.nix
     ./modules/desktop/alacritty.nix
     ./modules/desktop/rofi.nix
 
-    # Cli Modules
+    # cli modules
     ./modules/cli/zsh.nix
     ./modules/cli/git.nix
     ./modules/cli/nvim.nix
     ./modules/cli/tmux.nix
     
-    # App Modules
-    ./modules/app/pkgs.nix
+    # app modules
     ./modules/app/brave.nix
     ./modules/app/qemu.nix
 
@@ -37,9 +36,12 @@ in
 
 
 
-  home.file = {
-    
-  };
+  home.packages = [
+    pkgs.discord
+    pkgs.anki
+    pkgs.obsidian
+    pkgs.telegram-desktop
+  ];
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
