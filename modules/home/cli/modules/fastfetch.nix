92e56bd10c30bf76f48234e39e83cfbac2e7233a
mk <khlif.mk@proton.me>
2025-01-21 21:41:42 +0100

    dummy fastfetch initial config

--- modules/home/cli/modules/fastfetch.nix
+++ modules/home/cli/modules/fastfetch.nix
@@ -2,4 +2,12 @@
 
 {
   programs.fastfetch.enable = true;
+
+  home.file = {
+    ".config/fastfetch" = {
+      source = ../../_config/fastfetch;
+      recursive = true;
+      force = true; # Ensures the folder is replaced as a symlink
+    };
+  };
 }
